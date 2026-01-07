4825b7324acfe353c20bd9c5a46a46961c491942
Samiul Islam Ramim <123535324+SamiulIslamRamim@users.noreply.github.com>
2026-01-07 17:23:10 +0600

    initialized project

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="p-10 bg-shop-light-pink">
+      <h2 className="text-xl font-semibold">Home</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nesciunt
+        consequatur explicabo nam culpa, at qui, doloribus optio alias voluptas
+        natus vero laboriosam perspiciatis facere ipsam inventore, error dicta
+        repudiandae ut dolorem consequuntur libero corporis ea quia? Quisquam,
+        eos deserunt? Laudantium, amet ea rem odit vero deserunt labore magnam
+        praesentium?
+      </p>
+      <Button size="lg">Check out</Button>
+    </div>
+  );
+};
+
+export default Home;
